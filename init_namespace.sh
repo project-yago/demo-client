09b76a27dfffed313b6ee2fa7b62c37c237cd25c
Ronald de la REBERDIERE <delareberdiere.ronald@gmail.com>
2020-01-31 14:23:50 +0100

    **[Kayser][Demo][Demo Client]****[Kayser][Demo][Demo Client]** Init namespace

--- init_namespace.sh
+++ init_namespace.sh
@@ -2,5 +2,19 @@
 
 NAMESPACE=$1
 
-NAMESPACE_ID=$(curl --location --request PUT 'http://localhost:58080/namespaces' --header 'Content-Type: application/json' --data-raw '{"name": "${NAMESPACE}"}')
+NAMESPACE_ID=$(curl --location --request PUT 'http://localhost:58080/namespaces' --header 'Content-Type: application/json' --data-raw "{\"name\": \"$NAMESPACE\"}" | jq -r .id)
+
+echo "NAMESPACE_ID : ${NAMESPACE_ID}"
+
+CONFIG_URL="http://localhost:58080/agent/config/${NAMESPACE_ID}"
+
+echo "CONFIG_URL : ${CONFIG_URL}"
+
+curl --location --request PUT ${CONFIG_URL} \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "protocol": "http",
+  "proxyHost": "localhost",
+  "proxyPort": 55000
+}'
 
