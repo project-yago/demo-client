20a67aa2a6ae707765f56fdceec0f749f515081d
Ronald de la REBERDIERE <delareberdiere.ronald@gmail.com>
2022-09-22 06:11:26 +0200

    **[Kayser][Demo][Demo Client]** Java 17 Demo App

---
+++ app/java17/src/main/java/com/yago/app/demo/client/DemoJava17Application.java
@@ -0,0 +1,13 @@
+package com.yago.app.demo.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJava17Application.class, args);
+	}
+
+}
---
+++ app/java17/src/test/java/com/yago/app/demo/client/DemoJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yago.app.demo.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
