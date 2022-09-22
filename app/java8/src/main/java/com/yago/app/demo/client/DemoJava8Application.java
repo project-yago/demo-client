419fc40869601108d685a4018f2ae4b9f8fd3860
Ronald de la REBERDIERE <delareberdiere.ronald@gmail.com>
2022-09-22 05:43:26 +0200

    **[Kayser][Demo][Demo Client]** Java 8 Demo App

---
+++ app/java8/src/main/java/com/yago/app/demo/client/DemoJava8Application.java
@@ -0,0 +1,13 @@
+package com.yago.app.demo.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJava8Application.class, args);
+	}
+
+}
---
+++ app/java8/src/test/java/com/yago/app/demo/client/DemoJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yago.app.demo.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
