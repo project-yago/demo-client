38719889ca84631d3d8af6da92b8c91129cd61aa
Ronald de la REBERDIERE <delareberdiere.ronald@gmail.com>
2022-09-22 05:53:25 +0200

    **[Kayser][Demo][Demo Client]** Java 11 Demo App

---
+++ app/java11/src/main/java/com/yago/app/demo/client/DemoJava11Application.java
@@ -0,0 +1,13 @@
+package com.yago.app.demo.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJava11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJava11Application.class, args);
+	}
+
+}
---
+++ app/java11/src/test/java/com/yago/app/demo/client/DemoJava11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yago.app.demo.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJava11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
