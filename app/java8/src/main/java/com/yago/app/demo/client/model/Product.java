01928824c430620c166d0d8886ce528d8a0ccc70
Ronald de la REBERDIERE <delareberdiere.ronald@gmail.com>
2022-09-27 06:24:24 +0200

    **[Kayser][Demo][Demo Client]** Java 8 Demo App - Configuration - JSON Configuration

--- app/java8/src/main/java/com/yago/app/demo/client/model/Product.java
+++ app/java8/src/main/java/com/yago/app/demo/client/model/Product.java
@@ -2,6 +2,7 @@ package com.yago.app.demo.client.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -27,11 +28,15 @@ public class Product {
 
     private String category;
 
+    @JsonProperty("image_url")
     private String imageUrl;
 
+    @JsonProperty("home_url")
     private String homeUrl;
 
+    @JsonProperty("create_time")
     private String createTime;
 
+    @JsonProperty("update_time")
     private String updateTime;
 }
