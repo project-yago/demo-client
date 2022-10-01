78476db3994db5f473d5fa4f233c64129b8d7df4
hichemk <hichem.kaighani@gmail.com>
2022-10-01 14:05:00 +0200

    update productIT

--- app/java8/src/test/java/com/yago/app/demo/client/ProductIT.java
+++ app/java8/src/test/java/com/yago/app/demo/client/ProductIT.java
@@ -5,12 +5,10 @@ import com.yago.app.demo.client.model.Product;
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.MediaType;
 
-import java.util.UUID;
+//import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 
