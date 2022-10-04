05e29d9a15bf51cecfb2a399970bd3a5a89938ba
Ronald de la REBERDIERE <delareberdiere.ronald@gmail.com>
2022-10-04 15:07:18 +0200

    **[Kayser][Demo][Demo Client]** Java 8 Demo App - Profiles

--- app/java8/src/test/java/com/yago/app/demo/client/java8/DemoJava8ApplicationTests.java
+++ app/java8/src/test/java/com/yago/app/demo/client/java8/DemoJava8ApplicationTests.java
@@ -1,9 +1,11 @@
 package com.yago.app.demo.client.java8;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class DemoJava8ApplicationTests {
 
 	@Test
